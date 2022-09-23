a88b4c4fc005933560c64779e877168a6611b5cc
andy5995 <arch_stanton5995@protonmail.com>
2022-09-22 23:56:28 -0500

    entrypoint.sh:change some paths

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -e
 
-su user0ad --command "/home/user0ad/binaries/system/pyrogenesis -mod=$INPUT_NAME  \
-    -archivebuild=$PWD  \
-    -archivebuild-output=${INPUT_NAME}-${INPUT_VERSION}.pyromod \
-    -archivebuild-compress"
-
-# Output the filename
-cd ..
 ls -l
-mkdir -p /github/workspace/output/
-# Move the built package into the Docker mounted workspace
-mv -v *.{pyromod} /github/workspace/output/
+
+cd /github/workspace
+
+ls -al
+
+su user0ad --command "/home/user0ad/binaries/system/pyrogenesis -mod=$INPUT_NAME  \
+    -archivebuild=/github/workspace  \
+    -archivebuild-output=/github/workflow/${INPUT_NAME}-${INPUT_VERSION}.pyromod \
+    -archivebuild-compress"
