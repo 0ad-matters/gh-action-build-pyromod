8855b7793cfc03667a7ae289f482cb1aad871da6
andy5995 <arch_stanton5995@protonmail.com>
2022-09-23 00:13:29 -0500

    remove trailing slashes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 set -e
 
-ls -al
-
-ls -al /github/workflow
-
-su user0ad --command "/home/user0ad/binaries/system/pyrogenesis -mod=$INPUT_NAME  \
-    -archivebuild=$PWD  \
-    -archivebuild-output=\"/github/workflow/${INPUT_NAME}-${INPUT_VERSION}.pyromod\" \
+su user0ad --command "/home/user0ad/binaries/system/pyrogenesis -mod=$INPUT_NAME
+    -archivebuild=$PWD
+    -archivebuild-output=/github/workflow/${INPUT_NAME}-${INPUT_VERSION}.pyromod
     -archivebuild-compress"
