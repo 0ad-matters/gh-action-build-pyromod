022574e4db186f1b7a24760a1923d9b99a758180
andy5995 <arch_stanton5995@protonmail.com>
2022-09-23 00:15:10 -0500

    put cmd on one line

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 set -e
 
-su user0ad --command "/home/user0ad/binaries/system/pyrogenesis -mod=$INPUT_NAME
-    -archivebuild=$PWD
-    -archivebuild-output=/github/workflow/${INPUT_NAME}-${INPUT_VERSION}.pyromod
-    -archivebuild-compress"
+su user0ad --command "/home/user0ad/binaries/system/pyrogenesis -mod=$INPUT_NAME -archivebuild=$PWD -archivebuild-output=/github/workflow/${INPUT_NAME}-${INPUT_VERSION}.pyromod -archivebuild-compress"
