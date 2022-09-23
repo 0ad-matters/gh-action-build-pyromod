7c277f78d0efba02c62d0abda93552a39fff0ade
andy5995 <arch_stanton5995@protonmail.com>
2022-09-23 00:03:42 -0500

    try different paths

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -e
 
-ls -l
-
-cd /github/workspace
-
 ls -al
 
+ls -al /github/workflow
+
 su user0ad --command "/home/user0ad/binaries/system/pyrogenesis -mod=$INPUT_NAME  \
-    -archivebuild=/github/workspace  \
-    -archivebuild-output=/github/workflow/${INPUT_NAME}-${INPUT_VERSION}.pyromod \
+    -archivebuild=$PWD  \
+    -archivebuild-output=\"/github/workflow/${INPUT_NAME}-${INPUT_VERSION}.pyromod\" \
     -archivebuild-compress"
