6675cb14d7fa9b1b402a99f54e2b2e52fca8dadd
andy5995 <arch_stanton5995@protonmail.com>
2022-09-23 00:21:52 -0500

    entrypoint.sh:make output directory world-writable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 set -e
 
-su user0ad --command "/home/user0ad/binaries/system/pyrogenesis -mod=$INPUT_NAME -archivebuild=$PWD -archivebuild-output=/github/workflow/${INPUT_NAME}-${INPUT_VERSION}.pyromod -archivebuild-compress"
+export OUTPUT_DIR=/github/workspace/output
+mkdir -m 777 -p $OUTPUT_DIR
+
+su user0ad --command  "/home/user0ad/binaries/system/pyrogenesis  \
+    -mod=$INPUT_NAME    \
+    -archivebuild=$PWD  \
+    -archivebuild-output=${OUTPUT_DIR}/${INPUT_NAME}-${INPUT_VERSION}.pyromod    \
+    -archivebuild-compress"
