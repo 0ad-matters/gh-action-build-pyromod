6f9a5e3d796240dfb6ec2a16fee505646783061e
andy5995 <arch_stanton5995@protonmail.com>
2022-09-25 22:40:48 -0500

    remove debugging output

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@ set -ev
 OUTPUT_DIR=/github/workspace/output
 mkdir -m 777 -p $OUTPUT_DIR
 OUTPUT_FILE="$OUTPUT_DIR/$INPUT_NAME-$INPUT_VERSION.pyromod"
-echo "$OUTPUT_FILE"
 
 # pyrogenesis won't run this as root
 su user0ad --command  "/home/user0ad/binaries/system/pyrogenesis  \
@@ -13,5 +12,4 @@ su user0ad --command  "/home/user0ad/binaries/system/pyrogenesis  \
     -archivebuild-output=$OUTPUT_FILE    \
     -archivebuild-compress"
 
-ls "$OUTPUT_FILE"
 test -f "$OUTPUT_FILE"
