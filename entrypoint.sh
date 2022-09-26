0b35cff2b279e8d591d9b2c2dea8d5a8a7e9ae9d
andy5995 <arch_stanton5995@protonmail.com>
2022-09-25 22:32:54 -0500

    add temporary debug output

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ set -ev
 OUTPUT_DIR=/github/workspace/output
 mkdir -m 777 -p $OUTPUT_DIR
 OUTPUT_FILE="$OUTPUT_DIR/$INPUT_NAME-$INPUT_VERSION.pyromod"
+echo "$OUTPUT_FILE"
 
 # pyrogenesis won't run this as root
 su user0ad --command  "/home/user0ad/binaries/system/pyrogenesis  \
@@ -12,8 +13,5 @@ su user0ad --command  "/home/user0ad/binaries/system/pyrogenesis  \
     -archivebuild-output=$OUTPUT_FILE    \
     -archivebuild-compress"
 
-# For some reason this delay might be needed on small mods otherwise
-# the "test" will fail.
-sleep 1s
-
+ls "$OUTPUT_FILE"
 test -f "$OUTPUT_FILE"
