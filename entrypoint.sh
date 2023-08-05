f738636e706c63cd0e8d6d5c83fa77fd50a69d44
sl5net <sl5softwarelab@gmail.com>
2023-08-05 15:56:58 +0200

    updated version of the script with some comments added

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 set -ev
 
+# Set the default input directory if not provided
 if [ -z "$INPUT_DIRECTORY" ]; then
     INPUT_DIRECTORY="."
 fi
 
+# Create the output directory
 OUTPUT_DIR=/github/workspace/output
 mkdir -m 777 -p $OUTPUT_DIR
+
+# Set the output file path
 OUTPUT_FILE="$OUTPUT_DIR/$INPUT_NAME-$INPUT_VERSION.pyromod"
 
-# pyrogenesis won't run this as root
-su user0ad --command "/home/user0ad/usr/bin/pyrogenesis  \
-    -mod=package_mod   \
-    -archivebuild=$INPUT_DIRECTORY  \
-    -archivebuild-output=$OUTPUT_FILE    \
+# Run pyrogenesis command as non-root user
+su user0ad --command "/home/user0ad/usr/bin/pyrogenesis \
+    -mod=package_mod \
+    -archivebuild=$INPUT_DIRECTORY \
+    -archivebuild-output=$OUTPUT_FILE \
     -archivebuild-compress" \
     && test -f "$OUTPUT_FILE"
 
+# Remove git-related files from the output file
 zip -d "$OUTPUT_FILE" ".git*"
 
+# Remove additional specified files from the output file
 for item in $INPUT_REMOVE_FROM_PYROMOD; do
   if [ -z "${item##*.git*}" ]; then
     continue
